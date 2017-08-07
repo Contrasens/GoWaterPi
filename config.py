a83a85a6c86b4f9a1fa514a6619932689f3212dc
Contrasens <dinu.sarbu@gmail.com>
2017-08-07 23:36:43 +0200

    Added config file

---
+++ config.py
@@ -0,0 +1,26 @@
+# Which board pin is the automatic pump control connected to?
+PUMP_AUTO_PIN = 4
+
+# Which board pin is the manual pump control connected to?
+PUMP_MANUAL_PIN = 27
+
+# Which board pin is the green LED connected to?
+GREEN_LED_PIN = 22
+
+# Which board pin is the yellow LED connected to?
+YELLOW_LED_PIN = 23
+
+# Which board pin is the switch for the manual mode connected to?
+SWITCH_MANUAL_PIN = 17
+
+# Is the pump plugged into the normally-on socket?
+PUMP_DEFAULT_ON = False
+
+# How long should the pump run when engaged (in seconds)?
+PUMP_TIME = 60
+
+# At what hour should the pump turn on?  An integer 0-23.
+PUMP_TIME_ON = 18
+
+# Which port should be used by the web-server?
+PORT = 18861
--- goWaterPi.py
+++ goWaterPi.py
@@ -1,14 +1,9 @@
 import RPi.GPIO as GPIO
 import time
 import datetime
-
-pinAuto = 4  # Controls the relay that triggers manually the water pump
-pinManual = 27  # Controls the relay that triggers automatically the water pump
-pinGreen = 22  # Manual mode
-pinYellow = 23  # Automatic mode
-
-switchMan = 17  # switch in MANUAL mode (when switch in this position, water pump goes on)
-switchAuto = 18  # switch in AUTO mode (pumping water only on schedule, sensor-based etc.)
+from config import (PUMP_AUTO_PIN, PUMP_MANUAL_PIN,
+                    GREEN_LED_PIN, YELLOW_LED_PIN,
+                    SWITCH_MANUAL_PIN)
 
 ledGreenStatus = GPIO.LOW
 ledYellowStatus = GPIO.HIGH
@@ -17,14 +12,14 @@ ledYellowStatus = GPIO.HIGH
 def setup():
     GPIO.setmode(GPIO.BCM)  # Numbers GPIOs by physical location
 
-    GPIO.setup(pinAuto, GPIO.OUT, initial=ledYellowStatus)  # Set LedPin's mode is output
-    GPIO.setup(pinManual, GPIO.OUT, initial=GPIO.LOW)
-    GPIO.setup(pinGreen, GPIO.OUT, initial=ledGreenStatus)  # Set LedPin's mode is output
-    GPIO.setup(pinYellow, GPIO.OUT, initial=ledYellowStatus)  # Set LedPin's mode is output
+    GPIO.setup(PUMP_AUTO_PIN, GPIO.OUT, initial=ledYellowStatus)  # Set LedPin's mode is output
+    GPIO.setup(PUMP_MANUAL_PIN, GPIO.OUT, initial=GPIO.LOW)
+    GPIO.setup(GREEN_LED_PIN, GPIO.OUT, initial=ledGreenStatus)  # Set LedPin's mode is output
+    GPIO.setup(YELLOW_LED_PIN, GPIO.OUT, initial=ledYellowStatus)  # Set LedPin's mode is output
 
     # Set switches mode to input, and Raspberry Pi's internal pull-up resistors to high level(3.3V)
-    GPIO.setup(switchMan, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-    GPIO.setup(switchAuto, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    GPIO.setup(SWITCH_MANUAL_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    #GPIO.setup(SWITCH_AUTO_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 
 # callback function to service the interrupt
@@ -43,28 +38,28 @@ def isrSwitch(ev=None):
 # turn on the relay for manual mode
 def setManual():
     print(str(datetime.datetime.now()), "Switch set to ""Manual")
-    GPIO.output(pinAuto, GPIO.HIGH)
-    GPIO.output(pinManual, GPIO.LOW)
+    GPIO.output(PUMP_AUTO_PIN, GPIO.HIGH)
+    GPIO.output(PUMP_MANUAL_PIN, GPIO.LOW)
 
 
 # turn on the relay for automatic mode
 def setAuto():
     print(str(datetime.datetime.now()), "Switch set to ""Auto")
-    GPIO.output(pinAuto, GPIO.LOW)
-    GPIO.output(pinManual, GPIO.HIGH)
+    GPIO.output(PUMP_AUTO_PIN, GPIO.LOW)
+    GPIO.output(PUMP_MANUAL_PIN, GPIO.HIGH)
 
 
 def setLeds():
     global ledGreenStatus
     global ledYellowStatus
-    GPIO.output(pinGreen, ledGreenStatus)
-    GPIO.output(pinYellow, ledYellowStatus)
+    GPIO.output(GREEN_LED_PIN, ledGreenStatus)
+    GPIO.output(YELLOW_LED_PIN, ledYellowStatus)
 
 
 def loop():
     # wait for raising and set bouncetime to prevent the callback function from being called multiple times
     # when the button is pressed
-    GPIO.add_event_detect(switchMan, GPIO.RISING, callback=isrSwitch, bouncetime=200)
+    GPIO.add_event_detect(SWITCH_MANUAL_PIN, GPIO.BOTH, callback=isrSwitch, bouncetime=300)
 
     while True:
         time.sleep(1)  # Don't do anything for 1 second
@@ -76,6 +71,7 @@ def destroy():
 
 
 if __name__ == '__main__':  # Program start from here
+    print "GoWaterPi started. Ctrl + C to interrupt."
     setup()
     try:
         loop()

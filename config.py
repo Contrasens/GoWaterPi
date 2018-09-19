c89c6feaa7d1c302a62a0e4fc729006391512ffc
Contrasens <dinu.sarbu@gmail.com>
2018-09-19 22:33:05 +0200

    Added pin for the external sensor. Improved the information included in the email body

--- config.py
+++ config.py
@@ -16,8 +16,11 @@ YELLOW_LED_PIN = 23
 # Which board pin is the switch for the manual mode connected to?
 SWITCH_MANUAL_PIN = 17
 
-# Which board pin is connected to the temperature sensor?
+# Which board pin is connected to the internal temperature sensor?
 TEMPERATURE_PIN = 25
+
+# Which board pin is connected to the external temperature sensor?
+TEMPERATURE_EXT_PIN = 14
 
 # Which board pin is connected to the soil humidity sensor 1?
 SOIL_SENSOR1_PIN = 18
@@ -54,9 +57,13 @@ EMAIL_TO_ADDRESS = 'dinu.sarbu@gmail.com'
 EMAIL_SUBJECT_AFTER_WATERING = '[GoWaterPi] Irrigation successful!'
 
 # Which body should I use after watering the plants?
-humidity, temperature = Adafruit_DHT.read_retry(11, TEMPERATURE_PIN)
-EMAIL_BODY_TEXT = 'Hi, the irrigation was successful.' + '\n\n'
-EMAIL_BODY_DATA = str(datetime.now()) + ': Temperature: ' + str(temperature) + '*C, Humidity: ' + str(humidity) + '%\n\n'
+hum_int, temp_int = Adafruit_DHT.read_retry(11, TEMPERATURE_PIN)
+hum_ext, temp_ext = Adafruit_DHT.read_retry(11, TEMPERATURE_EXT_PIN)
+EMAIL_BODY_TEXT = ('Hi,' + '\n\n' + 
+                   'your plants were irrigated for ' + str(PUMP_TIME) + ' seconds.' + '\n\n')
+EMAIL_BODY_DATA = ('Date and time:'  + str(datetime.now()) + '\n' + 
+                   'Internal temperature: ' + str(temp_int) + '*C, internal humidity: ' + str(hum_int) + '%\n' +
+                   'External temperature: ' + str(temp_ext) + '*C, external humidity: ' + str(hum_ext) + '%\n\n')
 EMAIL_BODY_SIGN = '\GoWaterPi :)'
 
 
